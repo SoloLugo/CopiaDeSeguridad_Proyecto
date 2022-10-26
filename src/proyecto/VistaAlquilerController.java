b907397debbbb77365c800ef5c8e6ef6d0518a67
SoloLugo <friogamer4@gmail.com>
2022-10-26 00:20:06 -0500

    mejor alquiler intento 1 (revisar)

---
+++ src/proyecto/VistaAlquilerController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package proyecto;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+/**
+ * FXML Controller class
+ *
+ * @author jorge
+ */
+public class VistaAlquilerController implements Initializable {
+
+    @FXML
+    private AnchorPane Alquiler;
+    @FXML
+    private Pane Pane1;
+    @FXML
+    private ImageView Gato;
+    @FXML
+    private Button BTN_regresar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void getBack(ActionEvent event) {
+    }
+    
+}
