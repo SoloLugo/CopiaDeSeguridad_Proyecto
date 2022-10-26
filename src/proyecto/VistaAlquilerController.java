5d4035c38f49523a96f3d7476bb95f6d15dfb2d4
SoloLugo <friogamer4@gmail.com>
2022-10-26 00:56:22 -0500

    prototipo ventana alquiler

--- src/proyecto/VistaAlquilerController.java
+++ src/proyecto/VistaAlquilerController.java
@@ -4,15 +4,20 @@
  */
 package proyecto;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,11 +27,11 @@ import javafx.scene.layout.Pane;
 public class VistaAlquilerController implements Initializable {
 
     @FXML
-    private AnchorPane Alquiler;
+    private AnchorPane Alquilar;
     @FXML
-    private Pane Pane1;
+    private Pane Panel1;
     @FXML
-    private ImageView Gato;
+    private ImageView gato;
     @FXML
     private Button BTN_regresar;
 
@@ -39,7 +44,19 @@ public class VistaAlquilerController implements Initializable {
     }    
 
     @FXML
-    private void getBack(ActionEvent event) {
+    private void getBack(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vista/vista0.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        stage.setTitle("Tienda");
+        stage.setScene(scene);
+        stage.show();
+
+        Stage myStage = (Stage)this.Alquilar.getScene().getWindow();
+        myStage.close();
     }
     
 }
